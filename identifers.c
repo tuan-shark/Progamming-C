aa11fa29882dd2f3602c729290a1100b6118b1c5
tuan-shark <tuannm.23bi14441@usth.edu.vn>
2025-03-05 22:10:55 +0700

    Add files via upload

---
+++ identifers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void func(){
+// func is identifers
+}
+int main(){
+    int val = 10;
+    // val also is identifiers
+}
+// actually identifers is only just a name we set for variables , function ..... to easily use
+/*
+RULES
+IDENTIFERS CAN CONTAIN:
+1. A-Z or a-z
+2.0-9
+3.underscore(_)
+ex ABC_90
+NOTICE : CAN NOT START WITH NUMBER
+CAN NOT CONTAIN SPACE 
+CAN NOT DECLARE LIKE KEYWORDS OF LANGUAGE LIKE int IF = 9
+*/
